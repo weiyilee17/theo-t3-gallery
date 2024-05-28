7f4c48c9b61570c2e1b3842c6aac997d05ea97bd
Weiyi Lee <weiyilee@Weiyide-iMac-2.local>
2024-05-28 16:46:09 +0800

    Actually linking the database to the UI

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [x] Make it deploy (w/ vercel)
 - [x] Scaffold basic ui with mock data
 - [x] Tidy up build process
-- [ ] Actually set up a database (w/ vercel postgres)
+- [x] Actually set up a database (w/ vercel postgres)
 - [ ] Attach database to UI
 - [ ] Add authentication (w/ clerk)
 - [ ] Add image upload
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,23 @@
+import { desc } from "drizzle-orm";
 import { db } from "~/server/db";
-import { posts } from "~/server/db/schema";
+import { images } from "~/server/db/schema";
 
 export const dynamic = "force-dynamic";
 
-const mockImageURLs = [
-  "https://utfs.io/f/bbf968e5-3d64-4dfd-9dac-c7b04272a457-1ez072.png",
-  "https://utfs.io/f/85249d85-2c22-4545-8582-78ddba88b3a5-170x77.png",
-  "https://utfs.io/f/5170bbd3-d07e-4a12-bbbc-b22f591fb6dd-184ijo.png",
-];
-
-const mockImages = mockImageURLs.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const postsData = await db.select().from(posts);
+  const imagesData = await db.select().from(images).orderBy(desc(images.id));
 
-  console.log(postsData);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map(({ id, url }) => (
-          <div key={id} className="w-48">
-            <img src={url} alt="" />
+        {imagesData.map(({ id, url, name }, index) => (
+          <div key={`${id}-${index}`} className="flex w-48 flex-col">
+            <img
+              src={url}
+              alt=""
+              className="aspect-video w-full object-cover"
+            />
+            <div>{name}</div>
           </div>
         ))}
       </div>
