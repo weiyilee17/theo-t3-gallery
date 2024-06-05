82052e4bb7bc9da4dfce8d896a9e9287faae556d
Weiyi Lee <weiyilee@Weiyide-iMac-2.local>
2024-06-05 10:42:01 +0800

    More cleanup on modal

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,5 +3,17 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView({ id }: { id: number }) {
   const image = await getImage(id);
 
-  return <img src={image.url} alt={image.name} className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0 ">
+      {/* Not using Image from next because it creates fake elements to prevent layout shift, while the image is uploaded by
+      the user, so we don't know these information */}
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} alt={image.name} className="object-contain" />
+      </div>
+
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
