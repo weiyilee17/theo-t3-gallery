4276365d6c467371629c3dcef6f8f7edb6991d80
Weiyi Lee <weiyilee17@gmail.com>
2026-07-22 17:29:43 +0800

    Chagne proxy back to middleware to avoid edge support problem

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
+// Using middleware rather than proxy because edge runtime fails to run the new proxy file
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
